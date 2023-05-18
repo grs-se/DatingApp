1c87e79c8cbd1dc02cc504e5f3e3bd137249a366
grs-se <george_ricesmith@hotmail.com>
2023-05-18 15:44:14 +0100

    Add authentication middleware

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -15,7 +15,6 @@ namespace API.Services
         public TokenService(IConfiguration config)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
-
         }
 
         public string CreateToken(AppUser user)
