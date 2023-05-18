041c44a5b001cd86782537201888f09e41d736b4
grs-se <george_ricesmith@hotmail.com>
2023-05-18 10:41:44 +0100

    Add DTOs and validation

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt {  get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
