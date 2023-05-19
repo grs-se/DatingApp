87e7809a22e76244129b21664a6e4a9d218387f7
grs-se <george_ricesmith@hotmail.com>
2023-05-19 11:33:05 +0100

    Add home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
