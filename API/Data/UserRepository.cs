c745ed292be294cb54adf7b9ffab64abfb3f2e78
grs-se <george_ricesmith@hotmail.com>
2023-05-21 16:13:06 +0100

    Add User Repository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext context;
+
+        public UserRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await this.context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await this.context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await this.context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            this.context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -3,46 +3,41 @@ using API.Extensions;
 using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
-internal class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(builder => builder.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
 {
-    private static async void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        builder.Services.AddApplicationServices(builder.Configuration);
-        builder.Services.AddIdentityServices(builder.Configuration);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        app.UseMiddleware<ExceptionMiddleware>();
-
-        app.UseCors(builder => builder.AllowAnyHeader()
-        .AllowAnyMethod()
-        .WithOrigins("https://localhost:4200"));
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        using var scope = app.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        try
-        {
-            var context = services.GetRequiredService<DataContext>();
-            await context.Database.MigrateAsync();
-            await Seed.SeedUsers(context);
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetService<ILogger<Program>>();
-            logger.LogError(ex, "An error occured during migration")
-        }
-
-        app.Run();
-    }
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
 }
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
+app.Run();
