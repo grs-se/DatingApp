f09aef9e2204b5fee4e6b70aabe5b29b40d81a7e
grs-se <george_ricesmith@hotmail.com>
2023-05-21 15:09:07 +0100

    Seeding data

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,11 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +28,20 @@ internal class Program
         app.UseAuthorization();
 
         app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedUsers(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetService<ILogger<Program>>();
+            logger.LogError(ex, "An error occured during migration")
+        }
 
         app.Run();
     }
