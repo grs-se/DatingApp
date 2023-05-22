dcb20170bb0c85f19b0d41ea809e08c70bea4bf8
grs-se <george_ricesmith@hotmail.com>
2023-05-22 15:26:09 +0100

    Add styles to cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent {
   @Input() member: Member | undefined;
