ae4c7ef9ca348ed47e55317bc57bf65a31cd6950
grs-se <george_ricesmith@hotmail.com>
2023-05-23 18:33:06 +0100

    Persisting changes in the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -30,6 +31,21 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+            // because it's an update, client should have information about what just updated
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            // nothing has been saved to database if we get to this line
+            return BadRequest("Failed to update user");
         }
     }
 }
