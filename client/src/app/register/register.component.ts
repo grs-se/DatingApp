8ba47079c03bec844ed96189476a61415aca36b7
grs-se <george_ricesmith@hotmail.com>
2023-05-24 22:10:37 +0100

    Validation feedback in register component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]),
       confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
     });
