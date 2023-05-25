5dbc1d6c9c6a69006a5d9add5c453c64b7d45674
grs-se <george_ricesmith@hotmail.com>
2023-05-25 17:20:46 +0100

    Add filtering to API

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -11,5 +11,8 @@
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
 
+        public string CurrentUsername { get; set; }
+        public string Gender { get; set; }
+
     }
 }
