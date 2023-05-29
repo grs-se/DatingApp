b26934babdfa7c2060f2c9c50a6d176f20dec3bc
grs-se <george_ricesmith@hotmail.com>
2023-05-29 19:57:09 +0100

    Add likes repository

---
+++ API/DTOs/LikeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class LikeDto
+    {
+        public int Id { get; set; }
+        public int UserName { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+    }
+}
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+    }
+}
