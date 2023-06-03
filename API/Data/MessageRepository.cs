c9ea990287052b1d83bc40522504baf143d5949e
grs-se <george_ricesmith@hotmail.com>
2023-06-03 16:27:07 +0100

    Get messages from the Repo

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -2,16 +2,20 @@
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-        public MessageRepository(DataContext context)
+        public MessageRepository(DataContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
         public void AddMessage(Message message)
         {
@@ -28,9 +32,25 @@ namespace API.Data
             return await _context.Messages.FindAsync(id);
         }
 
-        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new NotImplementedException();
+            var query = _context.Messages
+                .OrderByDescending(x => x.MessageSent)
+                .AsQueryable();
+
+            // see which container user interested in seeing
+            query = messageParams.Container switch
+            {
+                // get query for that particular container
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+            };
+            // then project that to a MessageDto
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+            // which we then return as a PagedList
+            return await PagedList<MessageDto>
+                .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
