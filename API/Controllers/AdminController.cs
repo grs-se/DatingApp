a55a7443169e8b55110aa8b20b63b636acede525
grs-se <george_ricesmith@hotmail.com>
2023-06-04 16:22:32 +0100

    Get users with roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await _userManager.Users
+                .OrderBy(u => u.UserName)
+                // from users we have related table called UserRoles, so we want to project out of that so that we not only access
+                // the user roles table but we also access its related table which is the Roles table.
+                // Anonymous object - no type safety, which is fine in this case
+                .Select(u => new
+                {
+                    u.Id,
+                    Username = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
