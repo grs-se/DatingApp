e69ad584586d10cc07f7f4f59fa70dc5bf517d1d
grs-se <george_ricesmith@hotmail.com>
2023-06-07 21:11:29 +0100

    Implement Unit of Work

---
+++ API/Data/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using API.Interfaces;
+
+namespace API.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IUserRepository UserRepository => throw new NotImplementedException();
+
+        public IMessageRepository MessageRepository => throw new NotImplementedException();
+
+        public ILikesRepository LikesRepository => throw new NotImplementedException();
+
+        public Task<bool> Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        IMessageRepository MessageRepository { get; }
+        ILikesRepository LikesRepository { get; }
+        Task<bool> Complete();
+        // Tells us if Entity FW is tracking anything that is being changed inside its transaction.
+        bool HasChanges();
+    }
+}
